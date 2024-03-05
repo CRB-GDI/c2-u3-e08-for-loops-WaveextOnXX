7b7c7b131b374dc808a80bea0db321c6938ef5e8
WaveextOnXX <145377312+WaveextOnXX@users.noreply.github.com>
2024-03-05 12:52:05 -0500

    finished problem one

---
+++ Exersice8!/Exersice8!/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exersice8_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("give me a max number");
+           int maxnumber= int.Parse(Console.ReadLine()); 
+         
+            for (int i = 0;
+                i <= maxnumber; i = i+2) 
+            { 
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
